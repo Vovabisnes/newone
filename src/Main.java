2c72cbaf1e5f9774be31ef3f081dccc6e21fc34f
Vovabisnes <vovawestland@gmail.com>
2022-09-24 11:56:27 +0200

    new Feature added

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello Deutschland");
+        System.out.println();
+    }
+
+    private static int getNum(int num){
+        return num;
     }
 }
